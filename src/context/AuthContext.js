dab01d4fc4cfbcfe1f82c409e10455031549d61b
HectorGitt <adeniyi.olaitanhector@yahoo.com>
2022-05-07 12:04:33 +0100

    added react-context for auth state

---
+++ src/context/AuthContext.js
@@ -0,0 +1,38 @@
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import {useContext,createContext, useState} from 'react';
+import { useEffect } from 'react/cjs/react.development';
+import auth from '../firebase';
+
+const AuthContext = createContext()
+
+export const useAuth = () => {
+  return useContext(AuthContext)
+}
+
+export default function AuthProvider({children}) {
+    const [currentUser, setCurrentUser] = useState("")
+
+    const signup = (email,password) => {
+      return createUserWithEmailAndPassword(auth, email, password)
+    }
+    const login = (email, password) => {
+      return signInWithEmailAndPassword(auth, email, password)
+    }
+    
+    useEffect(()=> {
+      const unsubscribe = auth.onAuthStateChanged(user =>{
+        setCurrentUser(user)
+      })
+      return unsubscribe; 
+    }, [])
+    const value = {
+      currentUser,
+      signup,
+      login,
+    };
+  return (
+    <AuthContext.Provider value={value}>
+        {children}
+    </AuthContext.Provider>
+  )
+}
