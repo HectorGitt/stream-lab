afbdc7b8028e6f323f4e6f4e3b212f6d9507446e
HectorGitt <adeniyi.olaitanhector@yahoo.com>
2022-05-20 14:10:54 +0100

    imported firebase apikey from .env file

--- src/firebase.js
+++ src/firebase.js
@@ -6,7 +6,7 @@ import {getAuth} from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC2H2377KQABr38tRItnfyIqts-fpseSbU",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKKET,
