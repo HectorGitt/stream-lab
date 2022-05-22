83b8e81bae5e8da675a9ffc7e57ec65a711f3d2d
HectorGitt <adeniyi.olaitanhector@yahoo.com>
2022-05-22 23:29:15 +0100

    dynamically render banner from tmdb

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,16 +1,17 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
+import tmdbConfig from "./config/tmdbConfig";
 
 import pic from "../cover/banner.jpg";
-const Banner = () => {
+const Banner = ({imgId}) => {
   return (
     <div className="bannerCont">
       <div className="banner">
         <div className="iconCont">
           <FontAwesomeIcon size="2x" className="bannerIcon" icon={faAngleUp} />
         </div>
-        <h1>COMMAND IN YOUR HAND</h1>
-        <img src={pic} alt={pic} />
+        <h1>{imgId[0].title}</h1>
+        <img src={tmdbConfig.originalImage(imgId[0].backdrop_path)} alt={pic} />
       </div>
     </div>
   );
